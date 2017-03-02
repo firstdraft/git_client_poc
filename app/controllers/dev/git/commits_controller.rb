44fe4680bbb33e0ab39d5b48884da272589b74b9
Raghu Betina <rbetina@gmail.com>
2017-03-02 16:32:05 -0600

    Redirect after commit

--- app/controllers/dev/git/commits_controller.rb
+++ app/controllers/dev/git/commits_controller.rb
@@ -4,5 +4,7 @@ class Dev::Git::CommitsController < ApplicationController
       `git add -A`
       `git commit -m "#{params[:title]}" -m "#{params[:description]}"`
     end
+
+    redirect_to dev_git_url, notice: "Changed committed."
   end
 end
