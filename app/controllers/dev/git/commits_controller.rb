d3ef69852030970ae953b118550ccf778b086cf8
Raghu Betina <rbetina@gmail.com>
2017-03-02 16:28:25 -0600

    Do commits work? -m

---
+++ app/controllers/dev/git/commits_controller.rb
@@ -0,0 +1,8 @@
+class Dev::Git::CommitsController < ApplicationController
+  def create
+    Dir.chdir(Rails.root) do
+      `git add -A`
+      `git commit -m "#{params[:title]} -m #{params[:description]}"`
+    end
+  end
+end
