6dd18a387a42f59030175d660742971487f035fa
Raghu Betina <rbetina@gmail.com>
2017-03-02 16:08:45 -0600

    Stuff

--- app/controllers/dev/git/commands_controller.rb
+++ app/controllers/dev/git/commands_controller.rb
@@ -1,12 +1,19 @@
 class Dev::Git::CommandsController < ApplicationController
   def log
     Dir.chdir(Rails.root) do
-      @status = `git status`
 
-      @diff = `diff2html --style=side --output=stdout`.
-        gsub("<h1>Diff to HTML by <a href=\"https://github.com/rtfpessoa\">rtfpessoa</a></h1>", "").
-        gsub("<a class=\"d2h-file-switch d2h-hide\">hide</a>", "").
-        gsub("<a class=\"d2h-file-switch d2h-show\">show</a>", "")
+      @current_branch = `git symbolic-ref --short HEAD`
+
+      diff = `git diff`
+
+      unless diff.blank?
+        @diff_html = `diff2html --style=side --output=stdout`.
+          gsub("<h1>Diff to HTML by <a href=\"https://github.com/rtfpessoa\">rtfpessoa</a></h1>", "").
+          gsub("<a class=\"d2h-file-switch d2h-hide\">hide</a>", "").
+          gsub("<a class=\"d2h-file-switch d2h-show\">show</a>", "")
+      else
+        @status = `git status`
+      end
 
       @branches = `git branch`
 
