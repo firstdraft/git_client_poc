d140896d4585ed1a50d2e0d8acc39aadbaec95a3
Raghu Betina <rbetina@gmail.com>
2017-03-02 15:30:05 -0600

    Starting to build out interface

--- app/controllers/dev/git/commands_controller.rb
+++ app/controllers/dev/git/commands_controller.rb
@@ -3,6 +3,11 @@ class Dev::Git::CommandsController < ApplicationController
     Dir.chdir(Rails.root) do
       @status = `git status`
 
+      @diff = `diff2html --style=side --output=stdout`.
+        gsub("<h1>Diff to HTML by <a href=\"https://github.com/rtfpessoa\">rtfpessoa</a></h1>", "").
+        gsub("<a class=\"d2h-file-switch d2h-hide\">hide</a>", "").
+        gsub("<a class=\"d2h-file-switch d2h-show\">show</a>", "")
+
       @branches = `git branch`
 
       @log = `git log --oneline --decorate --graph --all -30`
