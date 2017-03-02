37992a44e08cad2734164103a674a4a8f4845493
Raghu Betina <rbetina@gmail.com>
2017-03-02 16:58:58 -0600

    Better log

--- app/controllers/dev/git/commands_controller.rb
+++ app/controllers/dev/git/commands_controller.rb
@@ -17,7 +17,7 @@ class Dev::Git::CommandsController < ApplicationController
 
       @branches = `git branch`
 
-      @log = `git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit`
+      @log = `git log --branches --remotes --tags --graph --oneline --decorate --pretty=format:"%d %s - %cr"`
     end
   end
 end
