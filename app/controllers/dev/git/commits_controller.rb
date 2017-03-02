b60e7a1cf8cfebf1c50926cf7856dd773a05e040
Raghu Betina <rbetina@gmail.com>
2017-03-02 16:29:18 -0600

    Fix description
    Commits with longer description

--- app/controllers/dev/git/commits_controller.rb
+++ app/controllers/dev/git/commits_controller.rb
@@ -2,7 +2,7 @@ class Dev::Git::CommitsController < ApplicationController
   def create
     Dir.chdir(Rails.root) do
       `git add -A`
-      `git commit -m "#{params[:title]} -m #{params[:description]}"`
+      `git commit -m "#{params[:title]}" -m "#{params[:description]}"`
     end
   end
 end
