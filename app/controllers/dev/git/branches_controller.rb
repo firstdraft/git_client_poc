5c361ada4491dd7ad4993adf056a1065a15baf02
Raghu Betina <rbetina@gmail.com>
2017-03-02 18:29:32 -0600

    Three options for changes

--- app/controllers/dev/git/branches_controller.rb
+++ app/controllers/dev/git/branches_controller.rb
@@ -1,7 +1,7 @@
 class Dev::Git::BranchesController < ApplicationController
   def create
     Dir.chdir(Rails.root) do
-      @status = `git checkout -b #{params[:name]}`
+      @status = `git checkout -b #{params[:name].parameterize}`
 
       redirect_to dev_git_url, notice: "Created branch #{params[:name]}."
     end
