8a5dbfbedcffd0c6cc4df19085b4c9cb81a99f92
Raghu Betina <rbetina@gmail.com>
2017-03-03 13:43:08 -0600

    Finished history

--- app/controllers/dev/git/branches_controller.rb
+++ app/controllers/dev/git/branches_controller.rb
@@ -1,7 +1,12 @@
 class Dev::Git::BranchesController < ApplicationController
   def create
     Dir.chdir(Rails.root) do
-      @status = `git checkout -b #{params[:name].parameterize}`
+      if params[:commit_hash].present?
+        `git checkout #{params[:commit_hash].strip.reverse.chomp("#").reverse}`
+        `git checkout -b #{params[:name].parameterize}`
+      else
+        `git checkout -b #{params[:name].parameterize}`
+      end
 
       redirect_to dev_git_url, notice: "Created branch #{params[:name]}."
     end
